1be28f5bfcb398997decbef686644830e95f9772
alex21k <77678805+alex21k@users.noreply.github.com>
2021-03-14 22:46:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 exports.min = function min(array) {
+  if (Array.isArray(array) === true){
   let minimum=0;
-  for (let i=0; i< array.length; i++) {
-  if (typeof(array[i]) === "number" && minimum > array[i]){
+  let k = array.length;
+  for (let i=0; i< k; i++) {
+  if (typeof(array[i]) === "number" && minimum >= array[i]){
   minimum = array[i];
-  }
+  } 
+
 }
+return minimum;
+} else return 0;
 }
 
 exports.max = function max(array) {
+  if (Array.isArray(array) === true){
   let maximum=0;
   for (let i=0; i< array.length; i++) {
   if (typeof(array[i]) === "number" && maximum <= array[i]){
   maximum  = array[i];
-  } 
+  }  
   }
+  return maximum;
+}else return 0;
 }
 
 exports.avg = function avg(array) {
+  if (Array.isArray(array) === true && array.length !=0){
   let summ = 0;
+  
   for (let i=0; i < array.length; i++) {
-  if (typeof(array[i]) === "number")
+  if (typeof(array[i]) === "number") {
   summ +=array[i]; 
+  } else continue;
+  
   }
   let avg = summ/array.length;
-}
+return avg;
+} else return 0;
+} 
